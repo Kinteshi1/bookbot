46434780508ed4acbfc3a7411531358bdb0d9d52
Kinteshi1 <stephan.auer2002@gmail.com>
2025-04-04 11:24:05 +0200

    finish bot class

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+
+from dnf.util import empty
+
 from stats import(
     get_word_count,
     get_characters,
@@ -10,11 +14,17 @@ def get_book_text (file_path):
     return text
 
 def main():
-    file_path = 'books/frankenstein.txt'
+
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     book_text = get_book_text(file_path)
     word_count = get_word_count(book_text)
     characters_count = get_characters(book_text)
     characters_sorted = chars_dict_to_sorted_list(characters_count)
+
 
     print("============ BOOKBOT ============")
     print(f"Analyzing Book found at {file_path}...")
